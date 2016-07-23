dbc76d99c7f8a737367d24786c9b5b392b5d1742
Kellye Greene <kellye@gust.com>
2016-07-23 15:52:37 -0400

    Test wont run for some reason

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const app = "I don't do much."
 
-var dodger = document.getElementById('dodger')
+var dodger = document.getElementById('dodger');
 
 function moveDodgerLeft() {
-  var leftNumbers = dodger.style.left.replace('px', '')
-  var left = parseInt(leftNumbers, 10)
-
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
   if (left > 0) {
-    dodget.style.left = `${left - 1}px`
+    dodger.style.left = `${left-1}px`
   }
 }
 
 function moveDodgerRight() {
   var leftNumbers = dodger.style.left.replace('px', '')
-  var left = parseInt(leftNumbers, 10)
-
-  if (left < 400) {
-    dodger.style.left `${left + 1}px`
+  var left = parseInt(leftNumbers, 10);
+  if (left < 400 ) {
+    dodger.style.left = `${left + 1}px`
   }
 }
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft();
+  } else if (e.which === 39) {
+    moveDodgerRight();
+  }
+})
